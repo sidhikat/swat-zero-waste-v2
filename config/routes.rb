64511cac04a6cfdab29bd07fb52ae2140b279ec8
Laura Phelps <laphelps64@gmail.com>
2019-03-27 15:55:19 -0400

    moved nav to app

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,16 @@
 Rails.application.routes.draw do
+  resources :biographies
+  resources :extras
+  resources :galleries do
+    resources :images
+  end
+  resources :posters
+  resources :principals
+  resources :projects
+
   get 'home/index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'home#index'
+
+
 end
