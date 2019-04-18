37c495c676d887b2240a490629b85c98a039e899
husharon <husharon@me.com>
2019-04-18 17:04:31 -0400

    cskeleton of about us >> mission page; changed some routes. should be our story: mission, values, bios

--- config/routes.rb
+++ config/routes.rb
@@ -11,7 +11,8 @@ Rails.application.routes.draw do
   resources :projects
 
   get 'home/index'
-  get 'home/mission'
+  get 'ourstory/mission'
+  get 'ourstory/values'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'home#index'
 
