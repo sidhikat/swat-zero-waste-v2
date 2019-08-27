3126b43e97c4204aac7f1224c5f6b1edf86117f4
Sidhika Tripathee <sidhika.t@outlook.com>
2019-08-27 17:56:56 -0400

    fixed yml parsing error

--- config/boot.rb
+++ config/boot.rb
@@ -1,3 +1,5 @@
+require 'yaml' #fix yaml parsing error
+#YAML::ENGINE.yamler= 'syck'
 ENV['BUNDLE_GEMFILE'] ||= File.expand_path('../Gemfile', __dir__)
 
 require 'bundler/setup' # Set up gems listed in the Gemfile.
