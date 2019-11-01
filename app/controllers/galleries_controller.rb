3a1e270021273db7cb16165bc6a5678ac341af32
Jesse <jessejesse123@gmail.com>
2019-11-01 16:09:45 -0400

    Revert "Remove galleries controller"
    This reverts commit 2323fadc7f95bbe4ed22a84098abcc403f6e97ec.

---
+++ app/controllers/galleries_controller.rb
@@ -0,0 +1,49 @@
+class GalleriesController < ApplicationController
+
+  def index
+    @galleries = Gallery.all
+  end
+
+  def show
+    @gallery = Gallery.find(params[:id])
+    @images = @gallery.images.all
+  end
+
+  def new
+    @gallery = Gallery.new
+  end
+
+  def create
+    @gallery = Gallery.new(gallery_params)
+    if @gallery.save
+      redirect_to gallery_path(@gallery)
+    else
+      render 'new'
+    end
+  end
+
+  def edit
+    @gallery = Gallery.find(params[:id])
+  end
+
+  def update
+    @gallery = Gallery.find(params[:id])
+    if @gallery.update(gallery_params)
+      redirect_to gallery_path(@gallery)
+    else
+      render 'edit'
+    end
+  end
+
+  def destroy
+    @gallery = Gallery.find(params[:id])
+    @gallery.destroy
+
+    redirect_to galleries_path
+  end
+
+  private
+    def gallery_params
+      params.require(:gallery).permit(:title, :description)
+    end
+end
