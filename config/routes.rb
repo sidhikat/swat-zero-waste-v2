2ca38dc43e7859d3882fd3ecd7efa563f3289dfd
Sidhika Tripathee <sidhika.t@outlook.com>
2019-12-07 13:59:53 -0500

    added sync up page

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  get 'contact/index'
+
   devise_for :admin_users, ActiveAdmin::Devise.config
   ActiveAdmin.routes(self)
   resources :biographies
@@ -13,6 +13,7 @@ Rails.application.routes.draw do
   resources :ourstory
 
   get 'home/index'
+  get 'contact/index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'home#index'
 
