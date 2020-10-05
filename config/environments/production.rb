9aa0cf85aee6f775c7ee459af0573c1c63a0d6a0
Kevin Webb <kwebb@cs.swarthmore.edu>
2020-10-05 18:20:02 -0400

    Make background refer to precompiled assets, add production logging.

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -89,6 +89,10 @@ Rails.application.configure do
     config.logger    = ActiveSupport::TaggedLogging.new(logger)
   end
 
+  # KCW: logging path
+  Rails.logger = Logger.new(STDOUT)
+  config.logger = ActiveSupport::Logger.new("/var/log/szw/szw.log")
+
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
 end
